dcf7d730e489c3ae2bd8793bf533314df3c92697
sophiaxxx <sophia4619@gmail.com>
2022-12-27 15:41:15 +0800

    update

---
+++ socket-project/src/main/java/com/chu/demo/SocketProjectApplication.java
@@ -0,0 +1,13 @@
+package com.chu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketProjectApplication.class, args);
+	}
+
+}
